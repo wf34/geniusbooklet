4cd159080f9c2a6ec6a8d6c87a860d8c8288243e
wf34 <wf34@users.noreply.github.com>
2017-12-31 18:35:44 +0300

    [add] basic rendering of song in two styles: short and long

--- process_song.js
+++ process_song.js
@@ -31,21 +31,47 @@ function navigate_page(page) {
 }
 
 function fetch_page_content(page) {
-  console.log("Yay, page loaded")
+  console.log("Yay, page loaded");
   return global_page_handle.content();
 }
 
 
 function query_root_html(content) {
   console.log("got html");
-  const BODY_LYRICS_SELECTOR = 'body > routable-page > ng-outlet > song-page > div > div > div.song_body.column_layout > div.column_layout-column_span.column_layout-column_span--primary > div > defer-compile:nth-child(2) > lyrics > div > section > p';
+  const BODY_LYRICS_SELECTOR = "body > routable-page > ng-outlet > song-page > div > div > div.song_body.column_layout > div.column_layout-column_span.column_layout-column_span--primary > div > defer-compile:nth-child(2) > lyrics > div > section > p";
   return global_page_handle.$eval(BODY_LYRICS_SELECTOR, (el) => el.innerHTML);
 }
 
+
 function parse_lyrics(lyrics) {
-  console.log('got lyrics: ', lyrics);
-  return global_page_handle.setContent(lyrics);
+  const cheerio = require('cheerio')
+  const $ = cheerio.load(lyrics)
+  links = []
+  $('a').each(function(i, el) {
+    links.push($(this).prop('href'))
+    let p = $('<div>' + $(this).html() + '</div>');
+    p.attr('annotation_id', i)
+    $(this).replaceWith(p);
+    
+  });
+  console.log(links)
+  let short_song_html = $.html();
+  song_parts = []
+  song_annotations = []
+
+   const tabled_song = cheerio.load('<body><table></table></body>')
+  $('body').children().each(function(i, elm) {
+    console.log(elm.tagName)
+    if (elm.tagName === 'div') {
+      let annotation_id = $(this).attr('annotation_id')
+      tabled_song('table').append('<tr><td>' + $.html(elm) + '</td><td>' + links[annotation_id] + '</td></tr>')
+    } else {
+      tabled_song('table').append('<tr><td>' + $.html(elm) + '</td></tr>')
+    }
+  });
+  return global_page_handle.setContent(short_song_html + '<br>' + tabled_song('body').html());
 }
+
 
 function render() {
   return global_page_handle.pdf({path: destination, format: 'A4'});
@@ -57,7 +83,7 @@ function shutdown() {
 
 let follow_root_url = function (page) { return navigate_page(page, address); }
 
-let browser_promise = puppeteer.launch({headless: true})
+puppeteer.launch({headless: true})
   .then(instantiate_page, handle_error)
   .then(follow_root_url, handle_error)
   .then(fetch_page_content, handle_error)
